1af551779bd1165b36572fe8fdbd414f2c5311b5
Parth <pmdoshi1234@gmail.com>
2026-09-01 23:08:26 -0700

    Stack section: 29 tools as a three-ring orbit, not a list

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import Work from "@/components/Work";
+import Stack from "@/components/Stack";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <Hero />
       <Work />
+      <Stack />
       <About />
       <Contact />
     </>
